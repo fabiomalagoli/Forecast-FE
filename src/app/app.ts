be59a8040c687c6e05ad8f7b8bbe5c7a62dd0ba4
Gabri <gabrielecigarini@gmail.com>
2026-01-13 16:24:13 +0100

    Aggiunta dei componenti header e grid con stili e template associati

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './header/header'
+import { Grid } from './grid/grid';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, Grid],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/app/grid/grid.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-grid',
+  imports: [],
+  templateUrl: './grid.html',
+  styleUrl: './grid.css',
+})
+export class Grid {
+
+}
