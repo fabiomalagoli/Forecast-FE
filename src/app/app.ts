441994172e45f5468d4f4c7ec411134a90974ea4
Fabio Ubaldo <fabaf99@hotmail.it>
2026-01-14 16:07:07 +0100

    Creazione cartelle progetti e clienti.

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,13 +2,16 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './header/header'
 import { Grid } from './grid/grid';
+import { Progetti } from './progetti/progetti';
+import { Clienti } from './clienti/clienti';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Grid],
+  imports: [RouterOutlet, Header, Grid, Clienti, Progetti],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('Forecast-FE');
+  selectedPage = 'Home';
 }
--- src/app/header/sidebar/sidebar.ts
+++ src/app/header/sidebar/sidebar.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-sidebar',
@@ -7,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrl: './sidebar.css',
 })
 export class Sidebar {
+  @Output() slected = new EventEmitter<string>();
 
+  onClick(id: string) {
+    this.slected.emit(id);
+  }
 }
