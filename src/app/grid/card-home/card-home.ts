3825d4d36e32a4f07bed4967dcf2026cd414da8e
Mic-9 <116760683+Mic-9@users.noreply.github.com>
2026-01-15 17:55:43 +0100

    Aggiornamento layout e stile e aggiunto card component

---
+++ src/app/grid/card-home/card-home.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-home',
+  imports: [],
+  templateUrl: './card-home.html',
+  styleUrl: './card-home.css',
+})
+export class CardHome {
+
+}
--- src/app/grid/grid.ts
+++ src/app/grid/grid.ts
@@ -1,11 +1,10 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { CardHome } from './card-home/card-home';
 
 @Component({
   selector: 'app-grid',
-  imports: [],
+  imports: [CardHome],
   templateUrl: './grid.html',
   styleUrl: './grid.css',
 })
-export class Grid {
-
-}
+export class Grid {}
