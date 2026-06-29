41a7494c3b2754567a5997ca0eea0fdcea8c3f50
Gabri <gabrielecigarini@gmail.com>
2026-06-29 11:10:42 +0200

    Aggiunto collegamento sidebar alla pagina dei gruppi di lavoro. da implementare

---
+++ src/app/pages/work-groups/work-groups.component.ts
@@ -0,0 +1,15 @@
+import { Component, inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+
+
+@Component({
+  selector: 'app-work-groups',
+  standalone: true,
+  imports: [],
+  templateUrl: 'work-groups.component.html',
+  styleUrl: 'work-groups.component.scss'
+})
+
+export class WorkGroupsComponent {
+
+}
